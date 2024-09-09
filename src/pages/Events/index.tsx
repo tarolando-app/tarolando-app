5c75ed969240449c49cde39fa813fde1f59c4dd2
Bruno Matheus Novato Leal <brunonovato22@gmail.com>
2024-09-08 21:04:00 -0300

    Ajustes

--- src/pages/Events/index.tsx
+++ src/pages/Events/index.tsx
@@ -1,6 +1,4 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import EventDetails from "../EventDetails";
 import Home from "../Home";
 
